773ee924e6ddf54681ff8d3bd6c83dad33b3d780
binpang <1131252124@qq.com>
2020-05-23 14:57:37 -0400

    bash script updated

---
+++ bash_ehframe.sh
@@ -0,0 +1,24 @@
+input="/tmp/eh_frame_negative.log"
+while IFS= read -r line
+do
+	SUB=$(echo "$line" | cut -d':' -f 5)
+	NUM=$(echo "$SUB" | cut -d'#' -f 2)
+	path=$(echo "$line" | cut -d':' -f 1)
+	tmp=$(echo "$path" | cut -d'@' -f 3) 
+	res=${path//@//}
+	res=${res/./}
+	if [ $tmp = "utils" ]; then
+		file=$(echo "$path" | cut -d'@' -f 6)
+	else
+		file=$(echo "$path" | cut -d'@' -f 5)
+	fi
+	gt="gtRef_${file}"
+	gt="${gt}.pb"
+	pb=${res/$file/$gt}
+	#echo $res
+	#echo $pb
+	if [ $NUM = "0]" ]; then
+		echo "Result for $res"
+		./dyninstBB_extent $res $pb
+	fi
+done < "$input"
